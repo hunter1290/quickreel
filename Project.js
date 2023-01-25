2c5946ffc2cf5182684c465d637eef4fd94cf451
R K Sharma <77050199+rksharma007@users.noreply.github.com>
2023-01-25 23:31:00 +0530

    Update Project.js

---
+++ Project.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+    userid: {
+        type: mongoose.Types.ObjectId,
+        ref: 'userid'
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    videos: [
+        {
+            videoid: {
+                type: mongoose.Types.ObjectId,
+                ref: 'videoid'
+            },
+            trimstart: {
+                type: String,
+                default: '00:00:00'
+            },
+            trimend: {
+                type: String,
+                default: '00:00:00'
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Project = mongoose.model('project', ProjectSchema);
